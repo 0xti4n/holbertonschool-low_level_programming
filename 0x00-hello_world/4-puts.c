9d1d2feb7c1f6c230705ca5db7941813c6ad053a
Cristian Gomez <1154@holbertonschool.com>
2019-09-18 19:50:17 +0000

    puts only

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
-int main ()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
 
 {
 
-puts("\"programming is like building a multilingual puzzle");
-return 0;
+	puts("\"programming is like building a multilingual puzzle");
+	return 0;
 
 }
 
