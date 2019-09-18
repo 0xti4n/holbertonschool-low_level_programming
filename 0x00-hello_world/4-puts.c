35868f3f2267ec9b7452f021aa62e72c5f2fe12d
Cristian Gomez <1154@holbertonschool.com>
2019-09-18 19:58:22 +0000

    puts funtion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,13 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
+int main(void)
+{	
+	puts("\"Programming is like building a multilingual puzzle");
 
-int main (void)
-
-{
-
-	puts("\"programming is like building a multilingual puzzle");
-	return 0;
-
+	return (0);
 }
-
