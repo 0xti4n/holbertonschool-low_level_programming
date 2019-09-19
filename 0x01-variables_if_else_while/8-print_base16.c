eb9c0d41d6f3bca9eddb6d4061432463fee61381
Cristian Gomez <1154@holbertonschool.com>
2019-09-19 22:09:47 +0000

    Hexadecimal base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <='9'; ch++)
+	putchar(ch);
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
