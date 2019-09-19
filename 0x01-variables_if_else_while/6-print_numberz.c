0631df5faeb21f52c7b4aa95ac043f0d2a785dfb
Cristian Gomez <1154@holbertonschool.com>
2019-09-19 21:35:11 +0000

    Putchar int

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	putchar(a + '0');
+	putchar('\n');
+	return (0);
+}
