c2b685995c0d159fa7573a3957c4e22b55f690ef
Cristian Gomez <1154@holbertonschool.com>
2019-09-19 22:13:49 +0000

    Hexadecimal base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,12 @@ int main(void)
 {
 	int ch;
 
-	for (ch = '0'; ch <='9'; ch++)
+	for (ch = '0'; ch <= '9'; ch++)
 	putchar(ch);
 
 	for (ch = 'a'; ch <= 'f'; ch++)
 	putchar(ch);
 	putchar('\n');
+
 	return (0);
 }
