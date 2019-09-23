4470dc2be79e3806ba3986e74dcec92ba4f50a7a
Cristian Gomez <1154@holbertonschool.com>
2019-09-23 22:31:17 +0000

    putchar holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+int main(void)
+{
+	_putchar('h');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
