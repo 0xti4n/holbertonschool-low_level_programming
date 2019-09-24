57b4f6e04ec922251f66c1706095308ad5a365af
Cristian Gomez <1154@holbertonschool.com>
2019-09-24 03:34:22 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _abs(int b)
+{
+	if (b >= 0)
+	return (b);
+
+	else
+	return (-b);
+
+}
