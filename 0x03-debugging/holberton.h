a5a5636ad1b72f5610d1b2520573be0c8c9e5241
Cristian Gomez <1154@holbertonschool.com>
2019-09-25 03:34:18 +0000

    Prototype

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+largest_number(int a, int b, int c);
+print_remaining_days(month, day, year);
+convert_day(int month, int day);
+
+
+#endif /* HOLBERTON_H */
