01f675c10cd6ce6a9c71f4821182cdea5d02b55a
Cristian Gomez <1154@holbertonschool.com>
2019-09-25 23:20:53 +0000

    Print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_to_98 - prints number to 98
+ *@n: The character to print
+ *
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+	do {
+	if (n != 98)
+	printf("%d,", n);
+	else
+	printf("%d", n);
+	n++;
+	} while (n <= 98);
+	printf("\n");
+	}
+
+	else if (n == 98)
+	printf("%d\n", n);
+
+	else
+	{
+	do {
+	if (n != 98)
+	printf("%d,", n);
+	else
+	printf("%d", n);
+	n--;
+	} while (n >= 98);
+	printf("\n");
+	}
+
+}
