da6fa32394430e9bf4321848c1e2c9f7c963eb4e
Cristian Gomez <1154@holbertonschool.com>
2019-09-25 00:34:57 +0000

    Tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,24 +8,24 @@
 void times_table(void)
 {
         int row, column, res;
+	
+	for (row = 0; row <= 9; row++)
+	{
+	_putchar('0');
 
-        for (row = 0; row <= 9; row++)
-                {
-                     _putchar('0');
-  
-                for (column = 1; column <= 9; column++)
-                        {
-                        _putchar(',');
-                        _putchar(' ');
+	for (column = 1; column <= 9; column++)
+	{
+	_putchar(',');
+	_putchar(' ');
 
-                        res = row * column;
+	res = row * column;
 
-                        if (res <= 9)
-                        _putchar(' ');
-                        else
-                        _putchar((res / 10) + '0');
-                        _putchar((res % 10) + '0');
-                        }
-		_putchar('\n');
-                }
+	if (res <= 9)
+	_putchar(' ');
+	else
+	_putchar((res / 10) + '0');
+	_putchar((res % 10) + '0');
+	}
+	_putchar('\n');
+	}
 }
