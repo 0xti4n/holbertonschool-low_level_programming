ec183beb481a15b4e6905a429506054184f3ae2f
Cristian Gomez <1154@holbertonschool.com>
2019-09-30 19:15:06 +0000

    swaps two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+*swap_int-swaps the values of two intengers a and b
+*@a: The character to swap
+*@b: The second character to swap
+* Return: nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+
+}
