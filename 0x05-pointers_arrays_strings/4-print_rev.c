db72774c22fdfcd30be0e6d6dda7f4d1507da9c3
Cristian Gomez <1154@holbertonschool.com>
2019-10-01 16:01:07 +0000

    print the sentence in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+*print_rev - function to print the sentence in  reverse
+*@s: The character to print
+* Return: nothing.
+*/
+
+void print_rev(char *s)
+{
+	int i = 0, l;
+
+	while (s[i] != '\0')
+		i++;
+	{
+		for (l = i; l >= 0; l--)
+			_putchar(*(s + l));
+	}
+	_putchar('\n');
+}
