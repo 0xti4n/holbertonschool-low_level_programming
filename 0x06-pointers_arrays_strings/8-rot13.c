2f28e27242fd716519c237bf2885832b3f05e9cd
Cristian Gomez <1154@holbertonschool.com>
2019-10-06 18:05:02 +0000

    Encodes a string using rot 13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -5,23 +5,25 @@
 * @s: The string to encode
 * Return: (s) string
 */
-
 char *rot13(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
 		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
 		{
 			s[i] += 13;
 		}
-
+		
 		else
+		{
 			while ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
 			{
 				s[i] -= 13;
 			}
+		}
+		i++;
 	}
 	return (s);
 }
