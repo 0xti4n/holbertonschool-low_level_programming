d791e00f5158bc457d9c9aa7c5f677ca8015ae69
Cristian Gomez <1154@holbertonschool.com>
2019-10-09 00:57:30 +0000

    Finds the first occurrence of the substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+* _strstr - locates a substring.
+* @haystack: A valid string.
+* @needle: String pattern that you want to search inside haystack.
+* Return: The equal pattern.
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *Begin = haystack;
+		char *pattern = needle;
+
+		while (*haystack && *needle && *haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+		if (!*pattern)
+		{
+			return (Begin);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
