3d92911ec6b9cbeb821da4daeeca4b90f834996c
Cristian Gomez <1154@holbertonschool.com>
2019-10-09 00:59:14 +0000

    Gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 
-/*
+/**
 * _strspn - Gets the length of a prefix substring.
 * @s: This is the main C string to be scanned.
 * @accept: This is the string containing the list of characters to match in s.
