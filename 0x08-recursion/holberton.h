0a204982e8e2504f1eb4f21dcc1b0d66327678f0
Cristian Gomez <1154@holbertonschool.com>
2019-10-09 18:18:23 +0000

    Prototype

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-int _putchar(char c)
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
