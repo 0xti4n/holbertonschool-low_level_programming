7864a7644bdced5f45774f57e0051ee3eb39a557
Cristian Gomez <1154@holbertonschool.com>
2019-10-18 19:00:04 +0000

    Reallocates a memory block

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* _realloc - reallocates a memory block.
+* @ptr: Old size of memory.
+* @old_size: The size of reallocate.
+* @new_size: New size of memory.
+* Return: The new pointer and the new  allocation.
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	unsigned int i;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	p = malloc(new_size);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+
+	for (i = 0; i < old_size; i++)
+	{
+		p[i] = ((char *)ptr)[i];
+	}
+
+	free(ptr);
+	return (p);
+}
