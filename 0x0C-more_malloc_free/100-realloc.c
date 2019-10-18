e8a5cbd7b8fd00567aee6d9cf1ffbd46ce9a73c8
Cristian Gomez <1154@holbertonschool.com>
2019-10-18 19:33:49 +0000

    Reallocates a memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,17 +29,23 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (NULL);
 	}
-	
+
 	p = malloc(new_size);
 
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-	
-	for (i = 0; i < old_size || i < new_size; i++)
+
+	for (i = 0; i < old_size; i++)
 	{
-		p[i] = ((char *)ptr)[i];
+		if (i == new_size)
+		{
+			break;
+		}
+
+		else
+			p[i] = ((char *)ptr)[i];
 	}
 
 	free(ptr);
