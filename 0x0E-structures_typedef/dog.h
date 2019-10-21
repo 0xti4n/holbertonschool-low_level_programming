0b8cc378c8967fdee633b3ec2a4bb4c308476965
Cristian Gomez <1154@holbertonschool.com>
2019-10-21 19:10:49 +0000

    Prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,7 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog my_dog;
+typedef struct dog dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
