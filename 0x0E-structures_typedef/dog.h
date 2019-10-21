c6852044a0b28c035b07f961153a9c0da8dd05fd
Cristian Gomez <1154@holbertonschool.com>
2019-10-21 19:18:05 +0000

    Prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - New struct.
+* @name: The name of the dog.
+* @age: The age of the dog.
+* @owner: The name of the dog's owner
+*/
+
 struct dog
 {
 	char *name;
