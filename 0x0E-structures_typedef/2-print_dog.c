c85577104be28f3a5ce724dade26171f37844b3c
Cristian Gomez <1154@holbertonschool.com>
2019-10-21 21:02:03 +0000

    Prints a struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	{
 		if ((*d).name == NULL)
 		{
-			printf("nil\n");
+			printf("Name: (nil)\n");
 		}
 		else
 		{
@@ -23,7 +23,7 @@ void print_dog(struct dog *d)
 
 		if ((*d).owner == NULL)
 		{
-			printf("nil\n");
+			printf("Owner: (nil)\n");
 		}
 		else
 		{
