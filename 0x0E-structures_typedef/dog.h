502dff40789d2629dbfb6f25723c6a22c441c165
Cristian Gomez <1154@holbertonschool.com>
2019-10-21 19:09:56 +0000

    Prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog my_dog;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+
 
 #endif
