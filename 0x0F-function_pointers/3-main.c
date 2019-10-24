b16736d526fd173ad298aecd5b9b5485b5db8dae
Cristian Gomez <1154@holbertonschool.com>
2019-10-24 23:39:32 +0000

    Main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,20 +11,20 @@
 
 int main(int argc, char *argv[])
 {
-	int num1, num2; 
+	int num1, num2;
 	int calc;
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	if (argc != 3)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (!(*argv[2] == '+' && *argv[2] == '-' && *argv[2] == '*' 
-			&& *argv[2] == '/' && *argv[2] == '%'))
+	if ((*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
+			&& *argv[2] != '/' && *argv[2] != '%'))
 	{
 		printf("Error\n");
 		exit(99);
@@ -35,9 +35,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(100);
 	}
-	
-	calc = get_op_func(argv[2])(num1, num2);
+
+	calc = (get_op_func(argv[2])(num1, num2));
 	printf("%i\n", calc);
 	return (0);
-
 }
