7eb1ebbdc90ea10dca62c2bb682b173046cc51bc
Cristian Gomez <1154@holbertonschool.com>
2019-10-24 04:00:04 +0000

    Function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+* struct op - Struct op
+*
+* @op: The operator
+* @f: The function associated
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	
+	int i;
+
+	while (ops[i].op != NULL)
+	{
+		if (ops[i].op == s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
