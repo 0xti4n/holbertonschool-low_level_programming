7e52f76e3d7e1f0048298cdd113340bcbf5f8a09
Cristian Gomez <1154@holbertonschool.com>
2019-10-25 23:52:55 +0000

    Print anythhing

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* print_all - function that prints anything.
+* @format: The format to compare.
+* Return: Nothing
+*/
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int i = 0;
+	char *c;
+
+	va_start(list, format);
+
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+			printf("%c", va_arg(list, int));
+			break;
+
+			case 'i':
+			printf("%i", va_arg(list, int));
+			break;
+
+			case 'f':
+			printf("%f", va_arg(list, double));
+			break;
+
+			case 's':
+			c = va_arg(list, char *);
+			if (c == NULL)
+			{
+				c = "(nill)";
+			}
+			printf("%s", c);
+			break;
+		}
+		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
+					format[i] == 'f' || format[i] == 's'))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(list);
+}
