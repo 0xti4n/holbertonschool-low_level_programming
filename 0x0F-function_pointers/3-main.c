803a76cc472aa1001cdec6fa1a493ed8143a00b2
Cristian Gomez <1154@holbertonschool.com>
2019-10-26 01:49:57 +0000

    Operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,14 +14,15 @@ int main(int argc, char *argv[])
 	int num1, num2;
 	int calc;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
 	if ((*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
 			&& *argv[2] != '/' && *argv[2] != '%') || argv[2][1] != '\0')
@@ -30,7 +31,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && (num1 || num2) == 0)
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
