57e488b3b44a6860ddf4f45061a6f0c23b6fbcdc
Cristian Gomez <1154@holbertonschool.com>
2019-10-26 19:06:41 +0000

    Print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -40,9 +40,11 @@ void print_all(const char * const format, ...)
 			printf("%s", c);
 			break;
 		}
-		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
-					format[i] == 'f' || format[i] == 's'))
+		if ((format[i + 1] != '\0') && (format[i] == 'c' || format[i] == 'i'
+					|| format[i] == 'f' || format[i] == 's'))
+		{
 			printf(", ");
+		}
 		i++;
 	}
 	printf("\n");
