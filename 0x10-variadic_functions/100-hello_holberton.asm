773e4aa2368a01f8acf24ecfb252aeeaad4782cb
Cristian Gomez <1154@holbertonschool.com>
2019-10-27 04:16:08 +0000

    Assembly

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,38 +1,16 @@
-; hello_64.asm    print a string using printf
-; Assemble:	  nasm -f elf64 -l hello_64.lst  hello_64.asm
-; Link:		  gcc -m64 -o hello_64  hello_64.o
-; Run:		  ./hello_64 > hello_64.out
-; Output:	  cat hello_64.out
+section .rodata
+msg:    db 'Hello, Holberton', 10
+msglen: equ $-msg
 
-; Equivalent C code
-; // hello.c
-; #include <stdio.h>
-; int main()
-; {
-;   char msg[] = "Hello, Holberton\n";
-;   printf("%s\n",msg);
-;   return 0;
-; }
-		
-; Declare needed C  functions
-extern	printf		; the C function, to be called
-
-section .data		; Data section, initialized variables
-msg:	db "Hello, Holberton", 0	; C string needs 0
-fmt:    db "%s", 10, 0          ; The printf format, "\n",'0'
-
-section .text           ; Code section.
-
-global main		; the standard gcc entry point
-main:				; the program label for the entry point
-push    rbp		; set up stack frame, must be aligned
-							
-mov	rdi,fmt
-mov	rsi,msg
-mov	rax,0		; or can be  xor  rax,rax
-call    printf		; Call C function
-
-pop	rbp		; restore stack
-
-mov	rax,0		; normal, no error, return value
-ret			; return
+section .text
+global main
+main:
+; write(1, msg, msglen)
+mov rdi, 1
+mov rsi, msg
+mov rdx, msglen
+mov rax, 1
+syscall
+; return 0
+mov rax, 0
+ret
