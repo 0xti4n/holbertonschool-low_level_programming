fb63a6d9520cf786381b1f727640ff2cfd2534a9
Cristian Gomez <1154@holbertonschool.com>
2019-11-01 21:46:59 +0000

    Prints before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,8 +6,9 @@
 * Return: Nothing.
 */
 
-void before_main(void)__attribute__((constructor));
-void before_main(void)
+void __attribute__((constructor)) before(void);
+void before(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
