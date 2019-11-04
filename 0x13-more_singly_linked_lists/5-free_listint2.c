65d5a38a1339fa6305ec95699d2628e1026a446f
Holberton <holberton@Holbertons-iMac-7.local>
2019-11-04 14:59:06 -0500

    Free list2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_listint2 - Free a list.
+ * @head: The arguments.
+ * Return: The new node.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
