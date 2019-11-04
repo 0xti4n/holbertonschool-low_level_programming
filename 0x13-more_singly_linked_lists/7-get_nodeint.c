ea71bb3f1d123f164705955c35174c108d81d444
Cristian Gomez <1154@holbertonschool.com>
2019-11-04 22:40:09 +0000

    Return nth node of a list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,29 +3,30 @@
 #include <stdio.h>
 
 /**
- * pop_listint - Delete head node.
+ * get_nodeint_at_index - Return nth node of a list.
  * @head: The arguments.
- * Return: The value of the delete node.
+ * @index: The nodo to evaluate.
+ * Return: The value of the node.
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    listint_t *temp = head;
-    unsigned int c = 0;
+	listint_t *temp = head;
+	unsigned int c = 0;
 
-    if (head != NULL)
-    {
-        while (temp)
-        {
-            if (c == index)
-            {
-                return (temp);
-            }
-            temp = temp->next;
-            c++;
-        }
-        else
-        {
-            return (NULL);
-        }
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (temp)
+	{
+		if (c == index)
+		{
+			return (temp);
+		}
+		c++;
+		temp = temp->next;
+	}
+	return (0);
 }
