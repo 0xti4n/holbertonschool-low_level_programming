18d0e2e93ebea90aabf65e8288589552f1b77388
Holberton <holberton@Holbertons-iMac-7.local>
2019-11-04 16:17:56 -0500

    Test

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * pop_listint - Delete head node.
+ * @head: The arguments.
+ * Return: The value of the delete node.
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+    listint_t *temp = head;
+    unsigned int c = 0;
+
+    if (head == NULL)
+    {
+        return (NULL);
+    }
+
+    while (temp)
+    {
+        if (c == index)
+        {
+            return (temp);
+        }
+        temp = temp->next;
+        c++;
+    }
+}
