54e56cdb9d292b61139696bd8f01e771c4d98045
Holberton <holberton@Holbertons-iMac-7.local>
2019-11-05 15:59:36 -0500

    Delete node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * delete_nodeint_at_index - Inserts a new node at a given position.
+ * @head: The arguments.
+ * @index: The position of the node to delete.
+ * Return: 1 if it's succes or -1 if it's failed.
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp = *head;
+	listint_t *temp2 = (*head)->next;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = temp2;
+		free(temp);
+		return (1);
+	}
+
+	else
+	{
+		while (i < index - 1)
+		{
+			if (temp->next == NULL)
+				return (-1);
+			temp = temp->next;
+			temp2 = temp2->next;
+			i++;
+		}
+	}
+
+	if (temp2->next == NULL)
+	{
+		temp->next = NULL;
+		free(temp2);
+	}
+	else
+	{
+		temp->next = temp2->next;
+		free(temp2);
+	}
+	head = NULL;
+	return (1);
+}
