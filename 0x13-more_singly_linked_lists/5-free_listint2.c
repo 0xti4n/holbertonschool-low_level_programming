d1f808a4dd8bee775d55cbc5d5637746e58b354f
Holberton <holberton@Holbertons-iMac-7.local>
2019-11-05 10:01:30 -0500

    Free list2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,11 +12,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (*head == NULL)
+	{
+		return;
+	}
+
 	while (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
-		head = temp;
+		*head = temp;
 	}
-	head = NULL;
+	*head = NULL;
 }
