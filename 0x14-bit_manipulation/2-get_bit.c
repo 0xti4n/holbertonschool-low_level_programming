acf7eeada5e4cdca69a23fbc3a7241e3a93af3e3
Holberton <holberton@Holbertons-iMac-7.local>
2019-11-07 16:51:50 -0500

    Get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* get_bit - returns the value of a bit.
+* @n: The value
+* @index: The index.
+* Return: The bit.
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (n & (1 << index))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+	return (-1);
+}
