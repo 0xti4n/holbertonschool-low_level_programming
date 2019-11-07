f22de85c864aff1a4555681e6d52bd890184da47
Holberton <holberton@Holbertons-iMac-7.local>
2019-11-07 16:57:25 -0500

    Get a bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,14 +11,20 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (n & (1 << index))
+	if (index > 64)
 	{
-		return (1);
+		return (-1);
 	}
-
 	else
 	{
-		return (0);
+		if (n & (1 << index))
+		{
+			return (1);
+		}
+
+		else
+		{
+			return (0);
+		}
 	}
-	return (-1);
 }
