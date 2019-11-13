377fb07f2a89deb0494964eb7a96142d43c7f2b8
Holberton <holberton@Holbertons-iMac-7.local>
2019-11-12 19:31:24 -0500

    Copies

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "holberton.h"
+
+/**
+ * main - creates a file.
+ * @argc: New file.
+ * @argv: The content to write.
+ * Return: 1 if worked or -1 if a error ocurred.
+ */
+
+int main(int argc, char *argv[])
+{
+	int ifd, ofd, numread;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	ifd = open(argv[1], O_RDONLY);
+	if (ifd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		exit(98);
+	}
+	ofd = open(argv[2], O_CREAT | O_WRONLY, 0664);
+	if (ofd == -1)
+	{
+		close(ifd);
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+		exit(99);
+	}
+	while ((numread = read(ifd, buf, 1024)) > 0)
+	{
+		if (write(ofd, buf, numread) != numread)
+			dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+			exit(99);
+	}
+	if (numread == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		exit(98);
+	}
+
+	if (close(ifd) == -1 || close(ofd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
+		exit(100);
+	}
+	exit(EXIT_SUCCESS);
+}
