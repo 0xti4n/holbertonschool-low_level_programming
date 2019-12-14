796ee41ac78cd8bfd78b67d2416f37342e034fd1
Cristian Gomez <1154@holbertonschool.com>
2019-12-14 20:57:20 +0000

    8 task

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always EXIT_SUCCESS.
+*/
