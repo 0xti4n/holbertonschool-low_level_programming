663f8c01db26e0bb66011b1dcbafdbdc51b0d138
Cristian Gomez <1154@holbertonschool.com>
2019-12-14 23:26:26 +0000

    deletes the node at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -4,7 +4,97 @@
 #include "lists.h"
 
 /**
-* main - check the code for Holberton School students.
-*
-* Return: Always EXIT_SUCCESS.
+*get_nodeint_at_index - returns the nth node of a list.
+*@head: The list.
+*@index: The index to find.
+* Return: The nth node, or NULL.
 */
+
+dlistint_t *get_nodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp = head;
+	unsigned int i = 0;
+
+	if (head != NULL)
+	{
+		while (temp != NULL)
+		{
+			if (i == index)
+			{
+				return (temp);
+			}
+			temp = temp->next;
+			i++;
+		}
+	}
+	return (NULL);
+}
+
+
+/**
+*len - returns the number of elements in a linked list.
+*@h: The list to count.
+* Return: The count.
+*/
+
+unsigned int len(const dlistint_t *h)
+{
+	unsigned int c = 0;
+
+	while (h != NULL)
+	{
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
+
+
+/**
+*delete_dnodeint_at_index - Delete node.
+*@head: The nodes.
+*@index: NUmber to search.
+* Return: 1 succes or -1 if not.
+*/
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int len1;
+
+	len1 = len(*head);
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if (index == 0 && (*head)->next != NULL)
+	{
+		*head = (*head)->next;
+		free((*head)->prev);
+		(*head)->prev = NULL;
+		return (1);
+	}
+	if ((*head)->next == NULL)
+	{
+		free(*head);
+		*head = NULL;
+		return (1);
+	}
+
+	if (index == len1)
+	{
+		tmp = get_nodeint_at_index(*head, index);
+		tmp->prev->next = NULL;
+		free(tmp);
+		return (1);
+	}
+
+	else
+	{
+		tmp = get_nodeint_at_index(*head, index);
+		tmp->prev->next = tmp->next;
+		tmp->next->prev = tmp->prev;
+		free(tmp);
+		return (1);
+	}
+}
