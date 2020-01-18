2df7a528a4251d7fb548ce9b4b947f1d3518ddca
cristian gomez <1154@holbertonschool.com>
2020-01-18 21:47:16 +0000

    Get value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_get - function that retrieves a value associated with a key.
+* @ht: The hash table
+* @key: The key to find
+* Return: The value.
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	idx = key_index((unsigned char *)key, ht->size);
+
+	while (ht->array[idx])
+	{
+		if (strcmp(key, ht->array[idx]->key) == 0)
+			return (ht->array[idx]->value);
+		else
+			return (NULL);
+	}
+	return (NULL);
+}
