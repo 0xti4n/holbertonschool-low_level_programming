3bfac0b7bfc7ef9b529a9438cae3f17c78cc07eb
cristian gomez <1154@holbertonschool.com>
2020-01-18 23:17:12 +0000

    function that prints a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,15 +1,37 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_table_print - function that prints a hash table.
+ * @ht: the hash table to print.
+ * Return: nothing.
  */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned int long i = 0;
+	int flag = 0;
+	hash_node_t *tmp;
 
 	if (ht == NULL)
 		printf("{}\n");
-
+	printf("{");
+	while (i < ht->size)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			if (i > 0 && flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			tmp = tmp->next;
+			if (tmp != NULL)
+			{
+				flag = 0;
+				printf(", ");
+				continue;
+			}
+			flag = 1;
+		}
+		i++;
+	}
+	printf("}\n");
 }
