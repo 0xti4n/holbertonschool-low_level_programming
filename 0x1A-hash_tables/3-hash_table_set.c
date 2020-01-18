a428e3f743411910a92999a37a798fb0db78bf62
cristian gomez <1154@holbertonschool.com>
2020-01-18 20:23:46 +0000

    change the '\0'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,13 +43,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	idx = key_index((unsigned char *)key, ht->size);
 
-	if (ht->array[idx] == '\0')
+	if (ht->array[idx] == 0)
 	{
 		ht->array[idx] = add_node(&head, key, value);
 		return (1);
 	}
 
-	if (ht->array[idx] != '\0')
+	if (ht->array[idx] != 0)
 	{
 		tmp = head;
 		while (tmp->next != NULL)
