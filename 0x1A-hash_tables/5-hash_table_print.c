9d781854dfc58d321f6048bcbf47bdc2814a63e3
cristian gomez <1154@holbertonschool.com>
2020-01-18 22:26:18 +0000

    adding main and 5 task

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int long i = 0;
+
+	if (ht == NULL)
+		printf("{}\n");
+
+}
